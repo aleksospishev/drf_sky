3245400167a356d52e7221062add614a06c2966f
aleksospishev <aleksospishev@yandex.ru>
2025-03-24 10:07:18 +0300

    ci/cd second

--- drf_sky/settings.py
+++ drf_sky/settings.py
@@ -156,6 +156,6 @@ if "test" in sys.argv:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "test_db.sqlite3",
+            "NAME": str(BASE_DIR / "test_db.sqlite3"),
         }
     }
