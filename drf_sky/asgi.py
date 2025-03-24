698e2bbe3a53bf9a22d802d9b259bed656b1165b
aleksospishev <aleksospishev@yandex.ru>
2025-03-24 09:47:34 +0300

    ci/cd second

--- drf_sky/asgi.py
+++ drf_sky/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for drf_sky.yaml project.
+ASGI config for drf_sky project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "drf_sky.yaml.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "drf_sky.settings")
 
 application = get_asgi_application()
--- drf_sky/settings.py
+++ drf_sky/settings.py
@@ -43,7 +43,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "drf_sky.yaml.urls"
+ROOT_URLCONF = "drf_sky.urls"
 
 TEMPLATES = [
     {
@@ -61,7 +61,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "drf_sky.yaml.wsgi.application"
+WSGI_APPLICATION = "drf_sky.wsgi.application"
 
 
 DATABASES = {
